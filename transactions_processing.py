311a9bad1649fb63e713cbc76df77d7eeb3bdedd
Vaso Mtsituri <vasomtsituri@Vasos-MacBook-Pro.local>
2025-12-14 11:55:04 +0400

    feat(snowpark): implement transactions processing script to load data from row, transform and rewrite cleaned transactions back to Snowflake

--- transactions_processing.py
+++ transactions_processing.py
@@ -5,9 +5,10 @@ apply some transformations, and writes the results to a destination table.
 import json
 
 from snowflake.snowpark import Session
+from snowflake.snowpark.functions import bround, monthname, year
 
-SOURCE_TABLE = 'TRANSACTIONS_RAW.BANKS.TRANSACTIONS'
-DESTINATION_TABLE = 'TRANSACTIONS_RAW.BANKS.TRANSACTIONS_COUNTED_FROM_CF'
+SOURCE_TABLE = 'TRANSACTIONS_RAW.BANKS.BOG'
+DESTINATION_TABLE = 'TRANSACTIONS.BANKS.BOG'
 
 with open('conn_config.json', encoding='UTF-8') as f:
     connection_parameters = json.load(f)
@@ -15,6 +16,19 @@ with open('conn_config.json', encoding='UTF-8') as f:
 session = Session.builder.configs(connection_parameters).create()
 
 transactions = session.table(SOURCE_TABLE)
-transactions_count = transactions.group_by(transactions.ID).count()
 
-transactions_count.write.mode('overwrite').save_as_table(DESTINATION_TABLE)
+# Drop unnecessary columns
+transactions = transactions.drop(['COMMISSION_CCY', 'SETTLEMENT_CCY', 'BRAND_NAME'])
+
+# Round the settlement amount (column SETTLEMENT_AMOUNT) to 2 decimal places
+transactions = transactions.with_column('SETTLEMENT_AMOUNT',
+                                        bround(transactions['SETTLEMENT_AMOUNT'], 2))
+
+# Extract month and year from SETTLEMENT_DATE
+transactions = transactions.with_column('TRANSACTION_MONTH',
+                                        monthname(transactions['SETTLEMENT_DATE']))
+transactions = transactions.with_column('TRANSACTION_YEAR',
+                                        year(transactions['SETTLEMENT_DATE']))
+
+# Write the transformed data to the destination table
+transactions.write.mode('append').save_as_table(DESTINATION_TABLE)
