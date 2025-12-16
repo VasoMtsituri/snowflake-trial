068c94b429778bd9cbb9c4c86d3be0e8c9a8cbdc
Vaso Mtsituri <vasomtsituri@Vasos-MacBook-Pro.local>
2025-12-16 22:09:21 +0400

    feat(cloudbuild): implement internal staging creation and uploader script

---
+++ internal_staging/create_internal_stage.py
@@ -0,0 +1,24 @@
+import json
+
+from snowflake.core import Root
+from snowflake.core.stage import Stage, StageEncryption
+from snowflake.snowpark import Session
+
+DEFAULT_ENCRYPTION = 'SNOWFLAKE_SSE'
+BOG_TRANSACTIONS_STAGE = 'bog_transactions_stage'
+STAGE_DB = 'TRANSACTIONS_STAGE'
+STAGE_SCHEMA = 'BOG'
+
+with open('conn_config.json', encoding='UTF-8') as f:
+    connection_parameters = json.load(f)
+
+session = Session.builder.configs(connection_parameters).create()
+root = Root(session)
+
+bog_transactions_stage = Stage(
+    name=BOG_TRANSACTIONS_STAGE,
+    encryption=StageEncryption(type=DEFAULT_ENCRYPTION)
+)
+
+stages = root.databases[STAGE_DB].schemas[STAGE_SCHEMA].stages
+stages.create(bog_transactions_stage)
---
+++ internal_staging/load_data_into_internal_stage.py
@@ -0,0 +1,18 @@
+import json
+
+from snowflake.core import Root
+from snowflake.snowpark import Session
+
+DEFAULT_ENCRYPTION = 'SNOWFLAKE_SSE'
+BOG_TRANSACTIONS_STAGE = 'bog_transactions_stage'
+STAGE_DB = 'TRANSACTIONS_STAGE'
+STAGE_SCHEMA = 'BOG'
+
+with open('conn_config.json', encoding='UTF-8') as f:
+    connection_parameters = json.load(f)
+
+session = Session.builder.configs(connection_parameters).create()
+root = Root(session)
+
+my_stage_res = root.databases[STAGE_DB].schemas[STAGE_SCHEMA].stages[BOG_TRANSACTIONS_STAGE]
+my_stage_res.put('6554164645.csv', '/')
