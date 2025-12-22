f6aa5e58783a8d9fe5aace97ed3c98edb93a0f73
Vaso Mtsituri <vasomtsituri@Vasos-MacBook-Pro.local>
2025-12-22 21:57:11 +0400

    feat(alpha_vantage_dbt): implement DBT model alpha_vantage and its intermediate layer

---
+++ project_aether/dbt_project/models/intermediate/alpha_vantage_daily.sql
@@ -0,0 +1,11 @@
+SELECT
+    RAW_METADATA:"2. Symbol"::VARCHAR AS COMPANY,
+    RAW_METADATA:"3. Last Refreshed"::DATE AS LAST_REFRESHED,
+    f.key::DATE as TRADE_DATE,
+    f.value:"1. open"::NUMBER(19,4) as OPEN_PRICE,
+    f.value:"2. high"::NUMBER(19,4) as HIGH_PRICE,
+    f.value:"3. low"::NUMBER(19,4) as LOW_PRICE,
+    f.value:"4. close"::NUMBER(19,4) as CLOSE_PRICE,
+    f.value:"5. volume"::NUMBER(38,0) as VOLUME
+FROM alpha_vantage_stg_events,
+LATERAL FLATTEN(input => RAW_SERIES) f
